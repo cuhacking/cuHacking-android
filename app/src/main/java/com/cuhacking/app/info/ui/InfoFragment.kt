f382e466a38f34e36f0b505853e26a6a16432e21
derek <derek@llamabagel.ca>
2019-06-24 15:08:36 -0400

    Add basic screens and navigation

--- app/src/main/java/com/cuhacking/app/info/ui/InfoFragment.kt
+++ app/src/main/java/com/cuhacking/app/info/ui/InfoFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.crashlytics.android.Crashlytics
 
 import com.cuhacking.app.R
 
@@ -28,6 +30,14 @@ class InfoFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(InfoViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button).setOnClickListener {
+            Crashlytics.getInstance().crash()
+        }
     }
 
 }
--- app/src/main/java/com/cuhacking/app/ui/MainActivity.kt
+++ app/src/main/java/com/cuhacking/app/ui/MainActivity.kt
@@ -2,9 +2,14 @@ package com.cuhacking.app.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.cuhacking.app.R
+import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +20,23 @@ class MainActivity : AppCompatActivity() {
 
         val navController = findNavController(R.id.nav_host_fragment)
         findViewById<BottomNavigationView>(R.id.bottom_navigation_view).setupWithNavController(navController)
+
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+
+        toolbar.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.profile -> navController.navigate(R.id.profile)
+                R.id.admin -> navController.navigate(R.id.admin)
+            }
+
+            true
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        MenuInflater(this).inflate(R.menu.toolbar, menu)
+
+        return super.onCreateOptionsMenu(menu)
     }
 }
